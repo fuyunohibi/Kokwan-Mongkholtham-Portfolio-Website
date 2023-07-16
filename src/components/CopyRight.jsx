7639dc057f93fa2932139f2d32951da5e660cc4e
Ko-Kwan Mongkholtham <kokwan.m@gmail.com>
2023-07-16 12:22:54 +0700

    change copyright layout

--- src/components/CopyRight.jsx
+++ src/components/CopyRight.jsx
@@ -4,7 +4,7 @@ const CopyRight = ({ borderColor, textColor }) => {
    return (
       <div 
          name='copyright' 
-         className={`${borderColor ? borderColor : 'border-primary'} fixed left-[-5.1rem] transform rotate-90 translate-y-[29rem] smallMobile:translate-y-[22rem] border-b-4 z-[999] text-center leading-3 pl-[2px] breakpoint:left-[-7.18rem]`}
+         className={`${borderColor ? borderColor : 'border-primary'} fixed left-[-5.1rem] transform rotate-90 translate-y-[20rem] smallMobile:translate-y-[18rem] breakpoint:translate-y-[28rem] border-b-4 z-[999] text-center leading-3 pl-[2px] breakpoint:left-[-7.18rem]`}
       >
          <p 
             className={`${textColor ? textColor: 'text-primary'} text-[10px] font-japan tracking-[0.2em] mb-1 breakpoint:text-[14px]`}
